d1eee36a6836276367e371a8e61ef7b8c55a4ce5
ttay265 <tuannq265@gmail.com>
2019-03-19 23:16:49 +0700

    tuannq-add-shop-cofnig

--- www/controller/Transaction.controller.js
+++ www/controller/Transaction.controller.js
@@ -4,10 +4,10 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/Filter",
     "mortgage/pawnshop/model/formatter",
-    "sap/m/BusyDialog",
+    "sap/m/BusyDialog"
 ], function (BaseController, MessageToast, JSONModel, Filter, formatter, BusyDialog) {
     "use strict";
-
+    var url = "model/transactionDetail.json";
     return BaseController.extend("mortgage.pawnshop.controller.Transaction", {
         formatter: formatter,
         onInit: function () {
@@ -27,14 +27,46 @@ sap.ui.define([
         onCreateTransaction: function () {
             this.getRouter().navTo("creTrans");
         },
-        onTransDetailPress: function () {
+        onTransDetailPress: function (e) {
+            //fetch detail data of transaction
+            var transId = e.getSource().getBindingContext("trans").getProperty("/id");
+            this.getTransactionDetail(transId);
             if (!this.TransDetailDialog) {
                 this.TransDetailDialog = this.initFragment("mortgage.pawnshop.fragment.TransDetail");
             }
-            this.TransDetailDialog.getModel().setProperty("/editMode", false);
+
             this.TransDetailDialog.open();
         },
-        onTransEditPressed: function (oE) {
+        //load detail of transaction by transaction Id: d = transId
+        getTransactionDetail: function (transId) { //TransId: Mã HD
+            var ajaxData = {};
+            $.ajax({
+                url: url,
+                context: this,
+                dataType: 'json',
+                data: ajaxData,
+                success: function (d, r, xhr) {
+                    this.loadTransDetail(d);
+                },
+                error: function (e) {
+
+                }
+
+            });
+        },
+        //load data of trans detail to view (fragment TransDetail)
+        // d: data object
+        loadTransDetail: function (d) {
+            var transDetailModel = this.getModel("transDetail");
+            if (!transDetailModel) {
+                transDetailModel = new JSONModel(d);
+                this.setModel(transDetailModel, "transDetail");
+            } else {
+                transDetailModel.setProperty("/", d);
+            }
+            console.log(transDetailModel);
+        },
+        onTransEditPressed: function (e) {
             this.TransDetailDialog.getModel().setProperty("/editMode", true);
         }
     });

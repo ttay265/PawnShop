f83738966c94c924b3dc79f9a94062809d7f8549
ttay265 <tuannq265@gmail.com>
2019-04-10 21:16:57 +0700

    tuannq- dev - add - beautify

--- www/Component.js
+++ www/Component.js
@@ -11,7 +11,7 @@ sap.ui.define([
 
         metadata: {
             manifest: "json"
-        },
+        }, config: {fullWidth: true},
 
         /**
          * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
